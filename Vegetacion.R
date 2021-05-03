154f4a10e57e32ae1b0ae01ddf77d5b5dc68bcd5
Alejandro <franco.uriostegui1@gmail.com>
2021-05-03 11:03:34 -0500

    Argumentos extra en la función plot() para el cambio de símbolos y de color. Análisis del resultado cuando una variable categórica se tiene así, como factor(). Expresando un error.

--- Vegetacion.R
+++ Vegetacion.R
@@ -1,4 +1,35 @@
 vegetacion <- file('Vegetation2.txt')
 vegetacion <- read.table(vegetacion, header = TRUE)
 View(vegetacion)
-grafica_dispersion <- plot(vegetacion$BARESOIL, vegetacion$R)
+grafica_dispersion <- plot(x = vegetacion$BARESOIL, y = vegetacion$R)
+
+# Más especificaciones personalizar la gráfica.
+
+plot(vegetacion$BARESOIL, vegetacion$R,
+     xlab = 'Exposición del suelo',
+     ylab = 'Riqueza de especies', main = 'Gráfica de dispersión',
+     xlim = c(0,45), ylim = c(4,19),
+     pch = 15)
+
+# Cambio del color de los símbolos de acuerdo al tipo de transecto.
+
+vegetacion$Transect <- as.numeric(vegetacion$Transect)
+
+plot(vegetacion$BARESOIL, vegetacion$R,
+     xlab = 'Exposición del suelo',
+     ylab = 'Riqueza de especies', main = 'Gráfica de dispersión',
+     xlim = c(0,45), ylim = c(4,19),
+     pch = vegetacion$Transect,
+     col = vegetacion$Time)
+
+# Si la variable categórica 'Transecto' fuera un Factor. El cambio de simbolos
+# no sería posible.
+
+vegetacion$Transect <- factor(vegetacion$Transect)
+
+plot(vegetacion$BARESOIL, vegetacion$R,
+     xlab = 'Exposición del suelo',
+     ylab = 'Riqueza de especies', main = 'Gráfica de dispersión',
+     xlim = c(0,45), ylim = c(4,19),
+     pch = vegetacion$Transect,
+     col = vegetacion$Time)
