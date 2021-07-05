193b1c4b9123b91cb5032cecd6a9daa8765e7c15
Alex <franco.uriostegui@gmail.com>
2021-07-05 14:52:01 -0500

    Datos ordenados de la base de datos de toxinas para aplicarlos a la clase de factores. Documento guardado para compartir.

--- codigo_objetos_documento.R
+++ codigo_objetos_documento.R
@@ -12,6 +12,7 @@
 #Paquetes####
 library(knitr)
 library(dplyr)
+library(readxl)
 
 #Obtener ayuda####
 ayuda_fun <- c('help () o ?', 
@@ -55,3 +56,58 @@ morfometria <- read.csv('Morfometria_ejercicio.csv', header = TRUE)
 morfometria <- morfometria %>% 
         select(wingcrd, tarsus, head, Wt) %>% 
         slice(1:8)
+
+#Extraccion de bases####
+
+base_toxinas <- read_xlsx('Toxinas.xlsx',
+                          sheet = 1,
+                          na = 'NA')
+toxina_PLA2 <- base_toxinas %>% 
+        select(Species, PLA2) %>% 
+        mutate(Toxina = c('PLA2')) %>% 
+        filter(!is.na(PLA2)) %>% 
+        select(Species, Toxina)
+
+toxina_SVSP <- base_toxinas %>% 
+        select(Species, SVSP) %>% 
+        mutate(Toxina = c('SVSP')) %>% 
+        filter(!is.na(SVSP)) %>% 
+        select(Species, Toxina)
+
+toxina_SVMP <- base_toxinas %>% 
+        select(Species, SVMP) %>% 
+        mutate(Toxina = c('SVMP')) %>% 
+        filter(!is.na(SVMP)) %>% 
+        select(Species, Toxina)
+
+toxina_LAAO <- base_toxinas %>% 
+        select(Species, LAAO) %>% 
+        mutate(Toxina = c('LAAO')) %>% 
+        filter(!is.na(LAAO)) %>% 
+        select(Species, Toxina)
+
+toxina_3FT <- base_toxinas %>% 
+        select(Species, `3FT`) %>% 
+        mutate(Toxina = c('FT')) %>% 
+        filter(!is.na(`3FT`)) %>% 
+        select(Species, Toxina)
+        
+toxina_KUN <- base_toxinas %>% 
+        select(Species, KUN) %>% 
+        mutate(Toxina = c('KUN')) %>% 
+        filter(!is.na(KUN)) %>% 
+        select(Species, Toxina)
+
+toxina_CRiSP <- base_toxinas %>% 
+        select(Species, CRiSP) %>% 
+        mutate(Toxina = c('CRiSP')) %>% 
+        filter(!is.na(CRiSP)) %>% 
+        select(Species, Toxina)
+
+toxinas_todo <- bind_rows(toxina_LAAO, toxina_PLA2,
+                          toxina_SVMP, toxina_SVSP,
+                          toxina_3FT, toxina_KUN,
+                          toxina_CRiSP)
+
+toxinas_todo[,2] <- as.factor(toxinas_todo$Toxina)
+
